8ff3ffb8962d926204e32e2b7d55af63d8283edd
Wsy1524 <120700502+Wsy1524@users.noreply.github.com>
2023-03-01 20:40:47 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+//SHELL_LOCK
+    
+  async(data)
+        
+    load utils
+
+        //LOCK_INFO
+    
+  [id = async{document.fetch(async: *file*)}]
+
+const function: {async(;673654763676437637783766434673448767348923309-09237834)}
+
+    var async: = //ASYNC+LOAD_UTILTS
+
+*TYPE {
+  FILE; DOCUMENT; PAGE
+}
+
+          *PUT{
+async() => {
+        function; shell_lock: {
+document.bind(find:window)
+}
+}}
+
+  //ASYNC CONVERT {
+      KIND:{ASYNC:[
+  axios.fetch("document.url/string")
+]}
+  }
+*IMPORT {
+?//AYSNC CONVERT = //SHELL LOCK
+}
